21536b34287dc42e07205c7a6a94dd0f3349280a
kevhunte <kevinmhunte@gmail.com>
2021-11-05 08:04:56 -0400

    scaffolding and setup

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 
 # root of the project. This is the file that should get run 
+
+def handler ():
+    # run the steps to prep data, create, train, and test model
+    pass
+
+if __name__ == '__main__':
+    # grab whatever cmd line args and pass into handler.
+    handler()
