e9c366a0eb0613dd4fa68ff7301fc0f22d69c0a0
kevhunte <kevinmhunte@gmail.com>
2021-11-06 08:37:18 -0400

    more setup

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 
 # root of the project. This is the file that should get run 
+import sys, json
 
-def handler ():
-    # run the steps to prep data, create, train, and test model
+"""
+ticker => name of the particular coin to generate a model for
+run_settings => tells the handler how to run
+run_all => pass in to generate for all coins
+"""
+def handler (ticker, run_settings, run_all=False):
+    print('starting up...', ticker, run_settings, run_all, sep='\n')
+    # prep data 
+    # create model 
+    # train model 
+    # test model
     pass
 
 if __name__ == '__main__':
     # grab whatever cmd line args and pass into handler.
-    handler()
+    coin = sys.argv[1] if len(sys.argv) > 1 else ''
+    run_all = True if '--run-all' in sys.argv else False
+    with open('config.json','r') as f:
+        run_settings = json.load(f)
+    handler(coin, run_settings, run_all=run_all)
